43cbab373b020e720ad3b7127f7ac15b742fecb2
abderrahmane <abderrahamane7@gmail.com>
2022-03-09 14:24:56 +0100

    minor change

--- app/libraries/getheaders.php
+++ app/libraries/getheaders.php
@@ -1,4 +1,4 @@
 <?php
-header("Access-Control-Access-Orign: *");
-header("Access-control-Access-Methods: GET");
+header("Access-Control-Allow-Orign: *");
+header("Access-control-Allow-Methods: GET");
 header("Content-type: application/json");
